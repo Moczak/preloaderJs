a8091af0314f8042346ea7c99790d24d9978eaa3
Daniel Moczak <47257951+Moczak@users.noreply.github.com>
2024-08-24 21:58:59 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://Moczak.github.io/preloaderJs/preload.svg';
+svgImage.src = 'https://Moczak.github.io/preloaderJs/bouncing-circles.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
